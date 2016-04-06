c6f544b47521c85143f8df0637eaaaa9ee3c34f6
Борис <boris-gavrishuk@mail.ru>
2016-04-06 18:57:02 +0300

    route remove console.log

--- app/js/services/RegisterService.js
+++ app/js/services/RegisterService.js
@@ -16,63 +16,96 @@ var registerService = function() {
                 if(controlFio(ev)){
                     name = ev.value;
                     updateRegisterUser(ev);
-                    $(ev).parent().removeClass("has-error");
+                    removeTooltip(ev);
                 }
                 else{
                     name = "";
-                    $(ev).parent().addClass("has-error");
+                    addTooltip(ev, 1);
                 }
             }
             else if(ev.getAttribute("data-bind") == "surname"){
                 if(controlFio(ev)){
                     surname = ev.value;
                     updateRegisterUser(ev);
-                    $(ev).parent().removeClass("has-error");
+                    removeTooltip(ev);
                 }
                 else{
                     surname = "";
-                    $(ev).parent().addClass("has-error");
+                    addTooltip(ev, 2);
                 }
             }
             else if(ev.getAttribute("data-bind") == "patronymic"){
                 if(controlFio(ev)){
                     patronymic = ev.value;
                     updateRegisterUser(ev);
-                    $(ev).parent().removeClass("has-error");
+                    removeTooltip(ev);
                 }
                 else{
                     patronymic = "";
-                    $(ev).parent().addClass("has-error");
+                    addTooltip(ev, 3);
                 }
             }
             else if(ev.getAttribute("data-bind") == "phone"){
-                phone = ev.value;
-                updateRegisterUser(ev);
+                if(controlPhone(ev)){
+                    phone = ev.value;
+                    updateRegisterUser(ev);
+                    removeTooltip(ev);
+                }
+                else{
+                    phone = "";
+                    addTooltip(ev, 4);
+                }
+
             }
             else if(ev.getAttribute("data-bind") == "email"){
-                email = ev.value;
-                updateRegisterUser(ev);
+                if(controlEmail(ev)){
+                    email = ev.value;
+                    updateRegisterUser(ev);
+                    removeTooltip(ev);
+                }
+                else{
+                    email = "";
+                    addTooltip(ev, 5);
+                }
             }
             else if(ev.getAttribute("data-bind") == "login"){
-                login = ev.value;
-                updateRegisterUser(ev);
+                if(controlAuth(ev)){
+                    login = ev.value;
+                    updateRegisterUser(ev);
+                    removeTooltip(ev);
+                    if((login.length < 6) && (login.length != 0)){
+                        addTooltip(ev, 8);
+                        login = "";
+                    }
+                }
+                else{
+                    removeTooltip(ev);
+                    login = "";
+                    addTooltip(ev, 6);
+                }
             }
             else if(ev.getAttribute("data-bind") == "pass"){
-                pass = ev.value;
-                updateRegisterUser(ev);
+                if(controlAuth(ev)){
+                    pass = ev.value;
+                    updateRegisterUser(ev);
+                    removeTooltip(ev);
+                    if ((pass.length < 6) && (pass.length != 0)){
+                        addTooltip(ev, 9);
+                        pass = "";
+                    }
+                }
+                else{
+                    removeTooltip(ev);
+                    pass = "";
+                    addTooltip(ev, 7);
+                }
             }
 
         },
         isValidated: function(){
-            if (name.length == 0 || surname.length == 0 || patronymic.length == 0
-                || phone.length == 0 || email.length == 0 || login.length == 0 || pass.length == 0) {
-                console.log("Вы заполнили не все поля");
-            }
-            else if (login.length < 6) {
-                console.log("Введите более длинный логин");
-            }
-            else if (pass.length < 6) {
-                console.log("Введите более длинный пароль");
+            if ((name.length == 0) || (surname.length == 0) || (patronymic.length == 0) ||
+                (phone.length == 0) || (email.length == 0) || (login.length == 0) || (pass.length == 0)){
+                alert("Некоторые поля не заполнены, либо заполнены не корректно");
             }
             else {
                 isValid = true;
@@ -81,3 +114,86 @@ var registerService = function() {
         }
     }
 }();
+
+var removeTooltip = function(ev){
+    $(ev).parent().removeClass("has-error");
+    $(ev).tooltip("destroy");
+};
+var addTooltip = function(ev, key){
+    $(ev).parent().addClass("has-error");
+    switch (key)
+    {
+        case 1:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Имя может содержать только символы русского и латинского алфавита"
+            }).tooltip("show");
+            break;
+        }
+        case 2:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Фамилия может содержать только символы русского и латинского алфавита"
+            }).tooltip("show");
+            break;
+        }
+        case 3:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Отчество может содержать только символы русского и латинского алфавита"
+            }).tooltip("show");
+            break;
+        }
+        case 4:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Номер телефона может содержать только цифры"
+            }).tooltip("show");
+            break;
+        }
+        case 5:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Email содержит недопустимые символы"
+            }).tooltip("show");
+            break;
+        }
+        case 6:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Логин содержит недопустимые символы"
+            }).tooltip("show");
+            break;
+        }
+        case 7:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Пароль содержит недопустимые символы"
+            }).tooltip("show");
+            break;
+        }
+        case 8:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Логин должен содержать не менее 6 символов"
+            }).tooltip("show");
+            break;
+        }
+        case 9:{
+            $(ev).tooltip({
+                trigger: "manual",
+                placement: "bottom",
+                title: "Пароль должен содержать не менее 6 символов"
+            }).tooltip("show");
+            break;
+        }
+    }
+};
--- app/js/services/Tests.js
+++ app/js/services/Tests.js
@@ -5,13 +5,63 @@ var controlFio = function(ev){
     var key = true, str = ev.value;
 
     for(var i = 0; i < str.length; i++){
-       if((str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) || (str.charCodeAt(i) >= 176 && str.charCodeAt(i) <= 239)){
+       if((str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122)
+           || (str.charCodeAt(i) >= 1040 && str.charCodeAt(i) <= 1103) || (str.charCodeAt(i) == 1025) || (str.charCodeAt(i) == 1105)){
            key = true;
        }
        else{
            key = false;
            break;
        }
+    }
+    return key;
+};
+
+var controlPhone = function(ev){
+    var key = true, str = ev.value;
+
+    for(var i = 0; i < str.length; i++){
+        if((str.charCodeAt(i) >= 48) && (str.charCodeAt(i) <= 57)){
+            key = true;
+        }
+        else{
+            key = false;
+            break;
+        }
+    }
+    return key;
+};
+
+var controlEmail = function(ev){
+    var key = true, str = ev.value;
+
+    for(var i = 0; i < str.length; i++){
+        if((str.charCodeAt(i) >= 64 && str.charCodeAt(i) <= 90) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122)
+            || (str.charCodeAt(i) == 46) || (str.charCodeAt(i) == 95) || (str.charCodeAt(i) == 45) ||
+            ((str.charCodeAt(i) >= 48) && (str.charCodeAt(i) <= 57))){
+            key = true;
+        }
+        else{
+            key = false;
+            break;
+        }
+    }
+    return key;
+};
+
+var controlAuth = function(ev){
+    var key = true, str = ev.value;
+
+    for(var i = 0; i < str.length; i++){
+        if((str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122)
+            || (str.charCodeAt(i) == 46) || (str.charCodeAt(i) == 95) || (str.charCodeAt(i) == 45) ||
+            ((str.charCodeAt(i) >= 48) && (str.charCodeAt(i) <= 57))){
+            key = true;
+        }
+        else{
+            key = false;
+            break;
+        }
     }
     return key;
 };
